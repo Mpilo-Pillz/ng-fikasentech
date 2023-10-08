15471f2cf1c8b3beaddde86334a4c58b6d0e8e02
mpilopillz <mpilo.dlamini@yahoo.com>
2023-10-08 13:16:13 +0200

    adding jumbotron component

---
+++ src/app/shared/components/jumbotron/jumbotron.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-jumbotron',
+  templateUrl: './jumbotron.component.html',
+  styleUrls: ['./jumbotron.component.scss']
+})
+export class JumbotronComponent {
+  @Input() jumbotronTitle: string
+  @Input() jumbotronContent: string
+  @Input() imageUrl: string
+  @Input() actionText: string
+}
