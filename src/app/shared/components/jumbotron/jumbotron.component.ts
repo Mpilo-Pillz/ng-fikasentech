01eb3c4a4d0e59fd203758828a0a3f8eee440450
mpilopillz <mpilo.dlamini@yahoo.com>
2023-10-08 13:17:39 +0200

    deleting empty css file

--- src/app/shared/components/jumbotron/jumbotron.component.ts
+++ src/app/shared/components/jumbotron/jumbotron.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-jumbotron',
-  templateUrl: './jumbotron.component.html',
-  styleUrls: ['./jumbotron.component.scss']
+  templateUrl: './jumbotron.component.html'
 })
 export class JumbotronComponent {
   @Input() jumbotronTitle: string
