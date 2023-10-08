e445455e998c7b6fb8e5edf541cdb2ad30082178
mpilopillz <mpilo.dlamini@yahoo.com>
2023-10-08 14:30:33 +0200

    adding environment file

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [HeaderComponent, JumbotronComponent],
+  declarations: [HeaderComponent, JumbotronComponent, CardComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, JumbotronComponent],
+  exports: [HeaderComponent, JumbotronComponent, CardComponent],
 })
 export class SharedModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    fikasentaniApiUrl: "https://fikasentani.fly.dev"
+}
