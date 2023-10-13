b91553fde15dcca5613d146e2d93812cd8329696
mpilopillz <mpilo.dlamini@yahoo.com>
2023-10-13 19:35:29 +0200

    adding input to card compoennt

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
 })
 export class CardComponent {
-
+@Input() imageUrl: string
 }
