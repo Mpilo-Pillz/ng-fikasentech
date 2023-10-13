9e2f4be8f4e1a1e70255cfdd0e935885f3ccfd04
mpilopillz <mpilo.dlamini@yahoo.com>
2023-10-13 19:36:11 +0200

    adding angular common

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PagesModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, PagesModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
