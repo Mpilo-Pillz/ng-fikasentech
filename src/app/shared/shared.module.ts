f8507b3b1cd62e7b8bd20b3f25e3483470f2c4e8
mpilopillz <mpilo.dlamini@yahoo.com>
2023-11-14 21:39:28 +0200

    adding card header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 import { CardComponent } from './components/card/card.component';
 import { PageRoutingModule } from '../pages/page-routing.module';
+import { SkeletonLoaderComponent } from './components/skeleton-loader/skeleton-loader.component';
+import { PageHeaderCardComponent } from './components/card/page-header-card/page-header-card.component';
 
 @NgModule({
-  declarations: [HeaderComponent, JumbotronComponent, CardComponent],
+  declarations: [HeaderComponent, JumbotronComponent, CardComponent, SkeletonLoaderComponent, PageHeaderCardComponent],
   imports: [CommonModule, PageRoutingModule],
-  exports: [HeaderComponent, JumbotronComponent, CardComponent],
+  exports: [HeaderComponent, JumbotronComponent, CardComponent, PageHeaderCardComponent],
 })
 export class SharedModule {}
