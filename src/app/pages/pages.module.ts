434fc43a2f947dfc08794ab1fafc4d4b233b8634
mpilopillz <mpilo.dlamini@yahoo.com>
2023-11-24 21:21:54 +0200

    creating projects page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { TrainingComponent } from './training/training.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [HomeComponent, TrainingComponent],
+  declarations: [HomeComponent, TrainingComponent, ProjectsComponent],
   imports: [CommonModule, SharedModule],
   exports: [HomeComponent],
 })
