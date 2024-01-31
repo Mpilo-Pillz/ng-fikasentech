2e0310864fcacfceb59f20eb74ececf06ba0ec32
mpilopillz <mpilo.dlamini@yahoo.com>
2024-01-31 21:07:08 +0200

    adding about page to route an to pages module

--- src/app/pages/page-routing.module.ts
+++ src/app/pages/page-routing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { TrainingComponent } from "./training/training.component";
-import { ProjectsComponent } from "./projects/projects.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TrainingComponent } from './training/training.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 
 const trainingRoutes: Routes = [
-    { path: 'training', component: TrainingComponent },
-    { path: 'projects', component: ProjectsComponent },
-  ];
-  
-  @NgModule({
-    imports: [RouterModule.forChild(trainingRoutes)],
-    exports: [RouterModule]
-  })
-  export class PageRoutingModule { }
+  { path: 'training', component: TrainingComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'about', component: AboutComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(trainingRoutes)],
+  exports: [RouterModule],
+})
+export class PageRoutingModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { TrainingComponent } from './training/training.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent, TrainingComponent, ProjectsComponent],
+  declarations: [
+    HomeComponent,
+    TrainingComponent,
+    ProjectsComponent,
+    AboutComponent,
+  ],
   imports: [CommonModule, SharedModule],
   exports: [HomeComponent],
 })
