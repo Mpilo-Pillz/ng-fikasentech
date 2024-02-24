6881757aece1db7d744472b73646bb5994755285
mpilopillz <mpilo.dlamini@yahoo.com>
2024-02-24 11:46:11 +0200

    testing the filter pipe

--- src/app/pipes/filter.pipe.spec.ts
+++ src/app/pipes/filter.pipe.spec.ts
@@ -1,8 +1,43 @@
 import { FilterPipe } from './filter.pipe';
 
 describe('FilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterPipe();
+  let pipe: FilterPipe;
+
+  beforeEach(() => {
+    pipe = new FilterPipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter items based on filterKey and filterValue', () => {
+    const items = [
+      { name: 'Thulani', age: 25 },
+      { name: 'Lindelwa', age: 30 },
+      { name: 'Sihle', age: 35 },
+    ];
+
+    const filterKey = 'name';
+    const filterValue = 'Thulani';
+
+    const result = pipe.transform(items, filterKey, filterValue);
+
+    expect(result).toEqual([{ name: 'Thulani', age: 25 }]);
+  });
+
+  it('should return an empty array if no items match the filter', () => {
+    const items = [
+      { name: 'Thulani', age: 25 },
+      { name: 'Lindelwa', age: 30 },
+      { name: 'Sihle', age: 35 },
+    ];
+
+    const filterKey = 'name';
+    const filterValue = 'Akhile';
+
+    const result = pipe.transform(items, filterKey, filterValue);
+
+    expect(result).toEqual([]);
   });
 });
