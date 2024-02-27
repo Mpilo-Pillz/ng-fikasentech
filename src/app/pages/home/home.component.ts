c52d3b817c6f60880128744a69f2cfb748870d79
mpilopillz <mpilo.dlamini@yahoo.com>
2024-02-27 21:20:06 +0200

    dividing skills into sections

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,7 +11,6 @@ export class HomeComponent {
   I like to keep an open mind. I started my career in the Quality Assurance (Testing) side of the Software Development Lifecycle (SDLC). 
   In the early parts of my career I was working as a Test Automation Engineer. 
   I switched to work on the Development side of the SDLC currently working as a Full Stack Web Developer. Outside of work hours, I am learning iOS Mobile development. I enjoy learning on Udemy and Pluralsite.`;
-  imageUrl =
-    'https://lh3.googleusercontent.com/pWGKsbC67uKkiC4xQKOPM4juc2DCUN9oqsBKqaqpRawSTFAeYfPTW-MhTnvFGEUJ3nMJoqsbY3x6rs_uqZb9nts9npWdjgUtI0W3jhDfYd9LuSa7DFj6q9R5apEXi0JyTiuSJqQV=w2400';
+  imageUrl = '../../../assets/images/mpilo.png';
   actionText = 'View My CV';
 }
